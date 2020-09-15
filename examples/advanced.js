920f4636db280b70cf03de5b8b1c13e749e693e1
Stuart Casarotto <stuart@dds.mil>
2020-09-15 08:19:03 -0400

    updated advanced example

--- examples/advanced.js
+++ examples/advanced.js
@@ -1,41 +1,42 @@
 const { calculate, symptoms, conditions } = require('../dist/index.js');
 
 // create shared hashing algorithm for input
-const hashKey = (v) => {
-  return v.replace(/[,".]/g,'').replace(/\s/g,'').toLowerCase()
+const hashKey = v => {
+  return v
+    .replace(/[,".]/g, '')
+    .replace(/\s/g, '')
+    .toLowerCase();
 };
 
 // create hash map for symptoms
 let symptomsHashMap = {};
-symptoms.forEach((v, i) => {
+symptoms.items.forEach((v, i) => {
   symptomsHashMap[hashKey(v.prompt)] = v.key;
   if (v.aliases != undefined) {
-    v.aliases.forEach((a) => {
+    v.aliases.forEach(a => {
       symptomsHashMap[hashKey(a)] = v.key;
-    })
+    });
   }
 });
 
 // create hash map for conditions
 let conditionsHashMap = {};
-conditions.forEach((v, i) => {
+conditions.items.forEach((v, i) => {
   conditionsHashMap[hashKey(v.prompt)] = v.key;
   if (v.aliases != undefined) {
-    v.aliases.forEach((a) => {
+    v.aliases.forEach(a => {
       conditionsHashMap[hashKey(a)] = v.key;
-    })
+    });
   }
 });
 
-let inputSymptoms = [
-  'Shortness of breath'
-];
+let inputSymptoms = ['Shortness of breath'];
 
-let newSymptoms = inputSymptoms.map((v) => symptomsHashMap[hashKey(v)]);
+let newSymptoms = inputSymptoms.map(v => symptomsHashMap[hashKey(v)]);
 
 let inputConditions = [];
 
-let newConditions = inputConditions.map((v) => conditionsHashMap[hashKey(v)]);
+let newConditions = inputConditions.map(v => conditionsHashMap[hashKey(v)]);
 
 const result = calculate({
   symptoms: newSymptoms,
